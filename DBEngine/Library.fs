6772473852fe648c9d8e6f7cb9870b19a5aa6ea2
O'Ryan Hedrick <ryebread8403@gmail.com>
2022-06-15 05:18:43 -0600

    redid types to avoid the weird recursions, added newEdge function

--- DBEngine/Library.fs
+++ DBEngine/Library.fs
@@ -2,17 +2,34 @@
 open System
 module DBEngine =
 
-  type Node = { Id : Guid ; Labels : string List }
+ 
+  type Node =
+    {
+      Id : Guid;
+      Labels : string list ;
+      Properties : Map<string,Object>
+    }
 
-  type Edge = { Left : Node ; Right : Node }
-
-  type GraphObject =
-    | Node 
-    | Edge 
-    | Property
-
-  type Property = { Object : GraphObject ; Data : string}
+  type Edge = 
+    {
+      Id : Guid;
+      Labels : string list ;
+      LeftNode : Node ;
+      RightNode : Node ;
+      Properties : Map<string,Object>
+    }
 
   let newNode labels : Node = 
-    let Node = { Id = Guid.NewGuid() ; Labels = labels }
+    let Node = { Id = Guid.NewGuid() ; Labels = labels ; Properties = Map.empty }
     Node
+
+  let newEdge labels left right : Edge =
+    let Edge = 
+      {
+        Id = Guid.NewGuid() ;
+        Labels = labels ;
+        LeftNode = left ;
+        RightNode = right ;
+        Properties = Map.empty
+      }
+    Edge
--- UnitTests/Tests.fs
+++ UnitTests/Tests.fs
@@ -10,3 +10,12 @@ let ``Test creating a Node`` () =
     let Nodelabels = testNode.Labels
     Assert.Equal<string List>(labels,Nodelabels)
     0
+
+[<Fact>]
+let ``Test creating an Edge`` () =
+    let bob = DBEngine.newNode ["person"]
+    bob.Properties.Add("name","Bob") |> ignore
+    let alice = DBEngine.newNode ["person"]
+    alice.Properties.Add("name","Alice") |> ignore
+    let friendship = DBEngine.newEdge ["Friendship"] bob alice
+    0
