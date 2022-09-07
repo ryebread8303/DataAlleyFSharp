77e5322e7df75ba1e6ad77df575f63b83ee756e7
O'Ryan Hedrick <ryebread8403@gmail.com>
2022-09-06 20:38:01 -0600

    added XML documentation

--- DBEngine/Library.fs
+++ DBEngine/Library.fs
@@ -2,7 +2,8 @@
 open System
 module DBEngine =
 
- 
+  ///<summary>The Node type holds data objects in the database</summary>
+  /// <remarks>Nodes contain three properties, the ID: Guid, the Lables : String list, and the Properties: Map sring,object</remarks> 
   type Node =
     {
       Id : Guid;
@@ -10,6 +11,7 @@ module DBEngine =
       Properties : Map<string,Object>
     }
 
+  ///Edges record the links or relationships between Nodes.
   type Edge = 
     {
       Id : Guid;
@@ -19,12 +21,21 @@ module DBEngine =
       Properties : Map<string,Object>
     }
 
+  ///Atoms are one Node and a list of all Edges leading from that Node to other Nodes.
   type Atom = Node * Edge list
 
+  ///<summary>newNode creates a new Node object.</summary>
+  /// <param name="labels">A list of strings to set as the labels for the Node.</param>
+  /// <returns>a new Atom object</returns>
   let newNode labels : Atom = 
     let Node = { Id = Guid.NewGuid() ; Labels = labels ; Properties = Map.empty }
     ( Node , List<Edge>.Empty )
 
+  ///<summary>Add a new Edge, or relationship, between two Nodes.</summary>
+  /// <param name="labels">A list of strings to set as the labels for the Edge.</param>
+  /// <param name="left">The left hand, or originating, Node.</param>
+  /// <param name="right">The right hand, or destination, Node.</param>
+  /// <returns>an Edge object</returns>
   let newEdge labels left right : Edge =
     let edge = 
       {
@@ -36,6 +47,11 @@ module DBEngine =
       }
     edge
 
+  ///<summary>create a relation between Atoms by adding an Edge to the left one</summary>
+  /// <param name="leftAtom">the originating Atom</param>
+  /// <param name="rightAtom">the target Atom</param>
+  /// <param name="labels">a string list of labels to apply to the new Edge</param>
+  /// <returns>an Atom with an updated Edge list</returns>
   let joinAtoms (leftAtom : Atom) (rightAtom : Atom) (labels : string list ) : Atom =
     let (leftNode, leftEdges) = leftAtom
     let (rightNode, rightEdges) = rightAtom
@@ -43,6 +59,7 @@ module DBEngine =
     let newEdges = join :: leftEdges
     (leftNode, newEdges)
 
+  ///given an Atom, return a list of it's Edges.
   let listEdges (atom : Atom) : Edge list =
     let ( _ , edges) = atom
     edges
